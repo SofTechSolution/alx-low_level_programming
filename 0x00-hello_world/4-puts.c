a9831f447ba6a462590e23b71eab83a7d10ceb8a
Yusuf Adamu <yusa133@yahoo.com>
2022-09-08 04:28:56 -0700

    Task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *
+ * main - Entry point in c
+ * Return 0 after printing the function
+*/
+int main(void)
+{
+	puts(""Programming is like building a multilingual puzzle");
+	return 0;
+}
