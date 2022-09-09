8e60d2ed3aa9e5cc86f83c7aa7e48a645bf0bd3b
Yusuf Adamu <yusa133@yahoo.com>
2022-09-09 15:55:10 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * codes for printing all posible combination of two d/t digits
+ *
+ * in assending order and seperation by a comma following by a space
+ *
+ * Return: Always 0 success
+ */
+int main(void)
+	
+	int digit1, digit2, digit3;
+		for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = diigit1 + 1; digit2 < 10; digit2++)
+		{
+		for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+		{
+		putchar ((digit1 % 10) + '0');
+		putchar ((digit2 % 10) + '0');
+		putchar ((digit3 % 10) + '0');
+
+		iif (digit1 == 7 && digit2 == 8 && digit3 == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
+		}
+		}
+		}
+putchar('\n');
+return (0);
+}
