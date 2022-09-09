62aa2a31a3b359be867af74a4d041e2f8819a064
Yusuf Adamu <yusa133@yahoo.com>
2022-09-09 14:42:45 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
+
 char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
@@ -18,7 +19,5 @@ for (base_16 = 97; base_16 <= 102; base_16++)
 {
 putchar (base_16);
 }
-
-putchar ('\n');
 return (0);
 }
