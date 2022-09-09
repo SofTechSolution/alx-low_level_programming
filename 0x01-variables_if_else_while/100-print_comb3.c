94b1480ed7218dca31c50cdd1faa7dbe9fb1fae5
Yusuf Adamu <yusa133@yahoo.com>
2022-09-09 15:12:53 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * codess for printing all posible combination of two d/t digit
+ *
+ * in assending order and seperated by a comma following by a space.
+ */
+int main(void)
+{
+	int digit1, digit2;
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	{
+	putchar((digit1 % 10 ) + '0');
+	putchar((digit2 % 10 ) + '0');
+	if (digit1 == 8 && digit2 == 9)
+		continue;
+	putchar(',');
+	putchar(',');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
