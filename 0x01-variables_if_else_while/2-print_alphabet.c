f6a6edfcf62a988a90333a506932de083942cd34
Yusuf Adamu <yusa133@yahoo.com>
2022-09-09 14:00:16 -0700

    Tasks 2-print_alphabet.c 3-print_alphabets.c 4-print_alphabt.c 5-print_numbers.c 6-print_numberz.c 7-print_tebahpla.c 8-print_base16.c 9-print_comb.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwayes 0 (succecc)
+ */
+
+int main(void)
+{
+char alphabet;
+for (alphabet = a; alphabet <= 'z'; alphabet++);
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
