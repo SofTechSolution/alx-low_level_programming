89c9bc3bd79c8621049b027813c0fe64978d2130
Yusuf Adamu <yusa133@yahoo.com>
2022-09-09 15:32:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
+ *main - entry point
+ *
  * codess for printing all posible combination of two d/t digit
  *
  * in assending order and seperated by a comma following by a space.
+ *
+ * Return: Always 0 success
  */
-
 int main(void)
 {
 	int digit1, digit2;
