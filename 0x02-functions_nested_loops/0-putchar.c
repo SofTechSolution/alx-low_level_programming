8705bcde0c3e557e5fca48de8a50b24750aa1681
Yusuf Adamu <yusa133@yahoo.com>
2022-09-13 14:17:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 *main - print putchar
 *
 *Return: always 0
 */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,8 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-	
+	_putchar('\n');	
 	return (0);
 }
-
