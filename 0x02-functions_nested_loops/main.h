6740fba116e75b550cce19c2a9c525d0a88deb63
Yusuf Adamu <yusa133@yahoo.com>
2022-09-13 14:15:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,10 +10,10 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-voidtimes_table(void);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r. int n);
+void num(int r, int n);
 
 #endif
