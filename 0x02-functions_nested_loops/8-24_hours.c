4a6ea4568a024b9015df5442f7497454abaf1c7d
Yusuf Adamu <yusa133@yahoo.com>
2022-09-13 17:39:45 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 	{
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0')
+	_putchar(hours_remainder + '0');
 	_putchar(';');
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
