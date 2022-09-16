1bbeb8fd127d28ba93718a621f2e52923a98eca4
Yusuf Adamu <yusa133@yahoo.com>
2022-09-16 12:57:23 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-if (n < 0)
-{
-putchar('-');
-num = -num;
-}
-if (num > 9)
-{
-print_number(num / 10);
-}
-putchar(num % 10 + '0');
+	if (n < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+		putchar(num % 10 + '0');
 }
