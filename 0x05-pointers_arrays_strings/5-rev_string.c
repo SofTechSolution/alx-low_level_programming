17ac2082d054c2e36deaebc188379033bbd37cec
Yusuf Adamu <yusa133@yahoo.com>
2022-09-19 13:50:10 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,14 +16,14 @@ void rev_string(char *s)
 	while (s[length1] != '\0')
 		length1++;
 
-	length2 = length1 - 1
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
 		"samalx" "xlamas"
-	tem = s[i];
-	s[i] = s[length2];
-	s[length2] = tmp;
-	length2 -= 1;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	
 	}
 }
