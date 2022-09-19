7f18e39c409d898521e0ee8cb2c4cfbd300e0896
Yusuf Adamu <yusa133@yahoo.com>
2022-09-19 13:19:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*rev_string -> reversing a string
+*@s: parameter s
+*/
+
+void rev_string(chhar *s)
+{
+	"sam" -> "mas"
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] i = '\0')
+		length++;
+
+	lenght2 =length1 -1
+	for (i = 0; i > length1 /2; i++)
+	{
+		"samal x" "xlamas"
+	tem = s[i];
+	s[i] = s[lenth2];
+	s[length2] = tmp;
+	lenth2 -= 1;
+	}
+}
