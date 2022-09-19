78e495c9da466303c7a4c02207231f08d73c6679
Yusuf Adamu <yusa133@yahoo.com>
2022-09-19 13:32:47 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,24 @@
 
 void rev_string(char *s)
 {
-	"sam" -> "mas"
+
 	char tmp;
 	int i, length1, length2;
 
 	length1 = 0;
 	length2 = 0;
 
-	while (s[length1] i = '\0')
+	while (s[length1] != '\0')
 		length++;
 
-	lenght2 =length1 -1
-	for (i = 0; i > length1 /2; i++)
+	lenght2 =length1 - 1
+	for (i = 0; i < length1 / 2; i++)
 	{
-		"samal x" "xlamas"
+		"samalx" "xlamas"
 	tem = s[i];
-	s[i] = s[lenth2];
+	s[i] = s[length2];
 	s[length2] = tmp;
-	lenth2 -= 1;
+	length2 -= 1;
+	
 	}
 }
