1eae3467f425cb2edb666eb3ef7b1b8c10647bdb
Yusuf Adamu <yusa133@yahoo.com>
2022-09-22 05:05:16 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,9 +17,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (i = 0; n1[i]; i++)
 
 	for (j = 0; n2[j]; j++)
-
+		;
 	if (i > size_r || j > size_r)
-
 		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
