6df036c4069c5498b4bc82c01c5ab3c240c817c6
Yusuf Adamu <yusa133@yahoo.com>
2022-09-26 22:06:27 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
+int size = n; /* only accept positive sizes */
+if (size > 0)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	s[i] = b;
+}
 	return (s);
 }
