ee92124e72a7e02fda0d6ff1f25875dd619e1135
Yusuf Adamu <yusa133@yahoo.com>
2022-09-26 22:36:27 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@
 * int prototypes()
 */
 
-int _putchar(char);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
