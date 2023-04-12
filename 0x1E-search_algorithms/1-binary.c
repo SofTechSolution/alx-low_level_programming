b5b47df8a59b39beca6cb50a36f71f4a8cf72df5
Yusuf Adamu <yusa133@yahoo.com>
2023-04-12 04:41:37 -0700

    1-binary.c, 2.0

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,63 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "search_algos.h"
 
 /**
- * struct listint_s - singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t;
+ * binary_search - search a value in to ordered array
+ * @array: integer array
+ * @size: number of elements in array
+ * @value: the value to search
+ * Return: the first index with value matched or -1 otherwise
+*/
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t ini, size_t end);
-int jump_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-int advanced_binary_rec(int *array, size_t ini, size_t end, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int binary_search2(int *array, int ini, int end, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-#endif
+int binary_search(int *array, size_t size, int value)
+{
+	size_t ini, end;
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	ini = 0;
+	end = size - 1;
+	i = (ini + end) / 2;
+	while (ini <= end)
+	{
+		print_array(array, ini, end);
+		i = (ini + end) / 2;
+		if (array[i] == value)
+			return (i);
+		else if (array[i] > value)
+		{
+			end = i - 1;
+		}
+		else
+		{
+			ini = i + 1;
+		}
+	}
+	return (-1);
+}
+
+/**
+ * print_array - search a value in to ordered array
+ * @array: integer array
+ * @ini: initial index to print
+ * @end: end index to print
+*/
+
+void print_array(int *array, size_t ini, size_t end)
+{
+	size_t i, sw = 0;
+
+	printf("Searching in array: ");
+
+	for (i = ini; i <= end; i++)
+	{
+		if (sw == 0)
+		{
+			printf("%d", array[i]);
+			sw = 1;
+		}
+		else
+			printf(", %d", array[i]);
+	}
+	printf("\n");
+}
